f15765c6c9c08cd5cbf1af30f54bd0feeed4d8cc
CaedenPH <caedenperelliharris@gmail.com>
2023-10-04 21:51:11 +0100

    test(wa-tor): Add unit tests

---
+++ WaTorTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WaTorTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WaTorTests")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("74feb0b2-731e-4dec-89c8-32233cd2828b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WaTorTests/UnitTest1.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using WaTorSimulation;
+
+
+namespace WaTorTests
+{
+    [TestClass]
+    public class EntityTests
+    {
+        [TestMethod]
+        public void EntityInitialization_Prey_CorrectValues()
+        {
+            var entity = new Entity(true, (0, 0));
+
+            Assert.IsTrue(entity.Prey);
+            Assert.AreEqual(entity.Coords, (0, 0));
+            Assert.IsTrue(entity.Alive);
+            Assert.AreEqual(entity.RemainingReproductionTime, 5);
+        }
+
+        [TestMethod]
+        public void EntityInitialization_Predator_CorrectValues()
+        {
+            var entity = new Entity(false, (0, 0));
+
+            Assert.IsFalse(entity.Prey);
+            Assert.AreEqual(entity.Coords, (0, 0));
+            Assert.IsTrue(entity.Alive);
+            Assert.AreEqual(entity.RemainingReproductionTime, 20);
+            Assert.AreEqual(entity.EnergyValue, 15);
+        }
+
+        [TestMethod]
+        public void EntityResetReproductionTime_Prey_ResetsTo5()
+        {
+            var entity = new Entity(true, (0, 0));
+            entity.RemainingReproductionTime = 0;
+
+            entity.ResetReproductionTime();
+
+            Assert.AreEqual(entity.RemainingReproductionTime, 5);
+        }
+
+        [TestMethod]
+        public void EntityResetReproductionTime_Predator_ResetsTo20()
+        {
+            var entity = new Entity(false, (0, 0));
+            entity.RemainingReproductionTime = 0;
+
+            entity.ResetReproductionTime();
+
+            Assert.AreEqual(entity.RemainingReproductionTime, 20);
+        }
+    }
+}
